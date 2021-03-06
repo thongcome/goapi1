27985e60d72f62965a3869fd452c4d89a15aa95d
thongcome <thongcome@hotmail.com>
2021-03-06 15:09:27 +0700

    struct

--- server.go
+++ server.go
@@ -8,14 +8,33 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type inventory struct {
+	id     string
+	status string
+	name   string
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/hello", helloHandler)
+
+	e.GET("/getTodos", getTodosHandler)
 	// e.Start(":1234")
 	port := os.Getenv("PORT")
 	log.Println("port", port)
 
 	e.Start(":" + port)
+
+}
+
+func getTodosHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[int]*inventory{
+		1: &inventory{
+			id:     "001",
+			status: "processing",
+			name:   "Notebook",
+		},
+	})
 
 }
 
